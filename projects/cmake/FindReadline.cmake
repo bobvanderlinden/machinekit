62818b295d3c7be1ca9eb67791484c7462e1ce43
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-13 20:46:04 +0100

    use correct naming convention for *_LIBRARIES
    Some of the executables and libraries weren't linking correctly to
    pkgconfig-found libraries. The alternativing naming convention of
    _LIBRARY and _LIBRARIES was confusing, so FindReadline.cmake was changed
    as well.

--- projects/cmake/FindReadline.cmake
+++ projects/cmake/FindReadline.cmake
@@ -26,22 +26,22 @@ find_path(Readline_INCLUDE_DIR
     HINTS ${Readline_ROOT_DIR}/include
 )
 
-find_library(Readline_LIBRARY
+find_library(Readline_LIBRARIES
     NAMES readline
     HINTS ${Readline_ROOT_DIR}/lib
 )
 
-if(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+if(Readline_INCLUDE_DIR AND Readline_LIBRARIES AND Ncurses_LIBRARY)
   set(READLINE_FOUND TRUE)
-else(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-  FIND_LIBRARY(Readline_LIBRARY NAMES readline)
+else(Readline_INCLUDE_DIR AND Readline_LIBRARIES AND Ncurses_LIBRARY)
+  FIND_LIBRARY(Readline_LIBRARIES NAMES readline)
   include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY )
-  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
-endif(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARIES )
+  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARIES)
+endif(Readline_INCLUDE_DIR AND Readline_LIBRARIES AND Ncurses_LIBRARY)
 
 mark_as_advanced(
     Readline_ROOT_DIR
     Readline_INCLUDE_DIR
-    Readline_LIBRARY
+    Readline_LIBRARIES
 )
