73a4ebf8d24696338982c1cc2912a76d8feb3be9
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-28 15:49:06 +0100

    travis: added deb building

---
+++ .travis/build_debs.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+
+PROOT_OPTS="-b /dev/shm -r ${ROOT}"
+if echo ${TAG} | grep -iq arm; then
+    PROOT_OPTS="${PROOT_OPTS} -q qemu-arm-static"
+fi
+
+case "${FLAV}" in
+   "posix") FLAV_OPTS="-p"
+   ;;
+   "rt_preempt") FLAV_OPTS="-r" 
+   ;;
+   "xenomai") FLAV_OPTS="-x" 
+   ;;
+   *) FLAV_OPTS="-prx" 
+   ;;
+esac
+
+export FLAV_OPTS
+
+proot ${PROOT_OPTS} -r ${ROOT} \
+    sh -exc 'cd /usr/src/; \
+         mkdir -p build/${MK_DIR}; \
+         cp -a machinekit build/${MK_DIR}; \
+         cd build/${MK_DIR}/machinekit; \
+         ./debian/configure ${FLAV_OPTS}; \
+         debuild -eDEB_BUILD_OPTIONS="parallel=${JOBS}" \
+            -us -uc -b -j${JOBS}'
+
+# print out the results
+ls -l ${ROOT}/usr/src/build/${MK_DIR}/*deb || true
